27965f49a8db6c7c0d8aea6e68a6d5535dcd63fa
salma77 <salmaabdelfatah77@gmail.com>
2021-07-25 10:25:34 +0200

    added chapter 1 problem 9

---
+++ Chapter1/src/nine.java
@@ -0,0 +1,18 @@
+package Chapter1.src;
+
+public class nine {
+    public static void main(String[] args) throws Exception {
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+    }
+
+    public static boolean isRotation(String s1, String s2) {
+        if (s1.length() == s2.length())
+            return isSubstring(s1 + s1, s2);
+        
+        return false;
+    }
+
+    static boolean isSubstring(String s1, String s2) {
+        return true;
+    }
+}
