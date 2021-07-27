6bcff630f4012e03b3d9725bb57e56b27ff1cc34
salma77 <salmaabdelfatah77@gmail.com>
2021-07-27 16:14:04 +0200

    added StackMin

---
+++ Chapter3/src/StackMin.java
@@ -0,0 +1,51 @@
+package Chapter3.src;
+
+import java.util.EmptyStackException;
+
+public class StackMin {
+    private static class NodeMin {
+        private int data;
+        private int min;
+        private NodeMin next;
+
+        public NodeMin(int data, int min) {
+            this.data = data;
+            this.min = min;
+        }
+    }
+
+    private NodeMin top;
+
+    public int getMin() {
+        return top.min;
+    }
+
+    public int pop() {
+        if (top == null)
+            throw new EmptyStackException();
+        int item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public void push(int item) {
+        int new_min = 0;
+        if (top == null)
+            new_min = item;
+        else
+            new_min = (item <= top.min) ? item : top.min;
+        NodeMin new_top = new NodeMin(item, new_min);
+        new_top.next = top;
+        top = new_top;
+    }
+
+    public int peek() {
+        if (top == null)
+            throw new EmptyStackException();
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return (top == null);
+    }
+}
--- Chapter3/src/StacksAndQueues.java
+++ Chapter3/src/StacksAndQueues.java
@@ -1,10 +1,15 @@
 package Chapter3.src;
-
-import java.util.*;
-
 public class StacksAndQueues {
-
     public static void main(String[] args) {
-
+        StackMin salma = new StackMin();
+        salma.push(3);
+        salma.push(1);
+        System.out.println(salma.getMin());
+        salma.pop();
+        salma.push(7);
+        System.out.println(salma.getMin());
+        salma.push(2);
+        salma.push(9);
+        System.out.println(salma.getMin());
     }
 }
