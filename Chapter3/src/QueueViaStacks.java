406de5dc8fa2fb29d8e38ebb0346e1da69f400a5
salma77 <salmaabdelfatah77@gmail.com>
2021-07-27 19:34:13 +0200

    added QueueViaStacks.java

---
+++ Chapter3/src/QueueViaStacks.java
@@ -0,0 +1,30 @@
+package Chapter3.src;
+
+import java.util.*;
+
+public class QueueViaStacks<T> {
+
+    private Stack<T> front, back;
+
+    QueueViaStacks() {
+        front = new Stack<T>();
+        back = new Stack<T>();
+    }
+
+    public void add(T item) {
+        back.push(item);
+    }
+
+    public T remove() {
+        if (front.isEmpty()) {
+            while (!back.isEmpty()) {
+                front.push(back.pop());
+            }
+        }
+        return front.pop();
+    }
+
+    public boolean isEmpty() {
+        return front.isEmpty() && back.isEmpty();
+    }
+}
--- Chapter3/src/StacksAndQueues.java
+++ Chapter3/src/StacksAndQueues.java
@@ -1,15 +1,26 @@
 package Chapter3.src;
 public class StacksAndQueues {
     public static void main(String[] args) {
-        StackMin salma = new StackMin();
-        salma.push(3);
-        salma.push(1);
-        System.out.println(salma.getMin());
-        salma.pop();
-        salma.push(7);
-        System.out.println(salma.getMin());
-        salma.push(2);
-        salma.push(9);
-        System.out.println(salma.getMin());
+        // StackMin salma = new StackMin();
+        // salma.push(3);
+        // salma.push(1);
+        // System.out.println(salma.getMin());
+        // salma.pop();
+        // salma.push(7);
+        // System.out.println(salma.getMin());
+        // salma.push(2);
+        // salma.push(9);
+        // System.out.println(salma.getMin());
+        
+        QueueViaStacks<Integer> salma = new QueueViaStacks<Integer>();
+        salma.add(10);
+        salma.add(12);
+        salma.add(16);
+        System.out.println(salma.remove());
+        salma.add(18);
+        System.out.println(salma.remove());
+        System.out.println(salma.remove());
+        System.out.println(salma.remove());
+        System.out.println(salma.isEmpty());
     }
 }
