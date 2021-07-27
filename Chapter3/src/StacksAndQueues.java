66b1c6fdf64bfe41c920c81c85248d766b6f1e76
salma77 <salmaabdelfatah77@gmail.com>
2021-07-27 19:55:32 +0200

    added sort function

--- Chapter3/src/StacksAndQueues.java
+++ Chapter3/src/StacksAndQueues.java
@@ -1,5 +1,23 @@
 package Chapter3.src;
+
+import java.util.Stack;
+
 public class StacksAndQueues {
+
+    public static void sort(Stack<Integer> s) {
+        Stack<Integer> temp = new Stack<Integer>();
+        while (!s.isEmpty()) {
+            int dummy = s.pop();
+            while (!temp.isEmpty() && temp.peek() > dummy) {
+                s.push(temp.pop());
+            }
+            temp.push(dummy);
+        }
+
+        while (!temp.isEmpty())
+            s.push(temp.pop());
+    }
+
     public static void main(String[] args) {
         // StackMin salma = new StackMin();
         // salma.push(3);
@@ -11,16 +29,28 @@ public class StacksAndQueues {
         // salma.push(2);
         // salma.push(9);
         // System.out.println(salma.getMin());
-        
-        QueueViaStacks<Integer> salma = new QueueViaStacks<Integer>();
-        salma.add(10);
-        salma.add(12);
-        salma.add(16);
-        System.out.println(salma.remove());
-        salma.add(18);
-        System.out.println(salma.remove());
-        System.out.println(salma.remove());
-        System.out.println(salma.remove());
-        System.out.println(salma.isEmpty());
+
+        // QueueViaStacks<Integer> salma = new QueueViaStacks<Integer>();
+        // salma.add(10);
+        // salma.add(12);
+        // salma.add(16);
+        // System.out.println(salma.remove());
+        // salma.add(18);
+        // System.out.println(salma.remove());
+        // System.out.println(salma.remove());
+        // System.out.println(salma.remove());
+        // System.out.println(salma.isEmpty());
+
+        Stack<Integer> salma = new Stack<Integer>();
+        salma.push(10);
+        salma.push(99);
+        salma.push(1);
+        salma.push(7);
+        sort(salma);
+        salma.push(4);
+        salma.push(3);
+        salma.push(22);
+        while (!salma.isEmpty())
+            System.out.println(salma.pop());
     }
 }
