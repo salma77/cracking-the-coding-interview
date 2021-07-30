e1ef4a5f0b4fba48d5faad2d67ebd27944192abe
salma77 <salmaabdelfatah77@gmail.com>
2021-07-30 19:58:02 +0200

    added node datastructure

--- DataStructures/Node.java
+++ DataStructures/Node.java
@@ -1,5 +1,35 @@
 package DataStructures;
 
 public class Node {
-    
+    private String name;
+    private Node left;
+    private Node right;
+
+    public String visit(Node x) {
+        return x.name;
+    }
+
+    public void preOrderTraversal(Node root) {
+        if (root != null) {
+            visit(root);
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+    }
+
+    public void inOrderTraversal(Node root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            visit(root);
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public void postOrderTraversal(Node root) {
+        if (root != null) {
+            postOrderTraversal(root.left);
+            postOrderTraversal(root.right);
+            visit(root);
+        }
+    }
 }
