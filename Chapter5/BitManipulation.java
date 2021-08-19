1c743a960588f08da2c9dab4eac16525feec9a27
salma77 <salmaabdelfatah77@gmail.com>
2021-08-19 21:58:35 +0200

    solved problems 5.3 and 5.4(this one using 3 different solutions)

--- Chapter5/BitManipulation.java
+++ Chapter5/BitManipulation.java
@@ -71,6 +71,241 @@ public class BitManipulation {
     }
 
     /**
+     * Function that returns count of the longest sequence of ones in a number given
+     * that we can only flip one bit from zero to one
+     * 
+     * @param num
+     * @return
+     */
+    public int countLongestSequence(int num) {
+        if (~num == 0)
+            return Integer.BYTES * 8;
+        int curr_len = 0;
+        int prev_len = 0;
+        int max_len = 1;
+        while (num != 0) {
+            if ((num & 1) == 1)
+                curr_len++;
+            else if ((num & 1) == 0) {
+                prev_len = (num & 2) == 0 ? 0 : curr_len;
+                curr_len = 0;
+            }
+            max_len = Math.max(prev_len + curr_len + 1, max_len);
+            num >>>= 1;
+        }
+        return max_len;
+    }
+
+    @Test
+    public void testCountLongestSequence() {
+        assertEquals(1, countLongestSequence(0));// 0
+        assertEquals(2, countLongestSequence(1));// 1
+        assertEquals(8, countLongestSequence(1775));// 11011101111
+        assertEquals(5, countLongestSequence(463)); // 111001111
+        assertEquals(3, countLongestSequence(2730));// 101010101010
+    }
+
+    /**
+     * Function to get the next smaller number with the same number of one bits
+     * (brute force solution)
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextSmallerBruteForce(int num) {
+        int ones = BitManipulationHelpers.countOnes(num);
+        while (num > 0) {
+            num--;
+            if (BitManipulationHelpers.countOnes(num) == ones)
+                return num;
+        }
+        return 0;
+    }
+
+    @Test
+    public void testGetNextSmallerBruteForce() {
+        assertEquals(75, getNextSmallerBruteForce(77));
+        assertEquals(1, getNextSmallerBruteForce(2));
+        assertEquals(5, getNextSmallerBruteForce(6));
+        assertEquals(4, getNextSmallerBruteForce(8));
+    }
+
+    /**
+     * Function to get the next larger number with the same number of one bits
+     * (brute force solution)
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextLargerBruteForce(int num) {
+        int ones = BitManipulationHelpers.countOnes(num);
+        while (num > 0) {
+            num++;
+            if (BitManipulationHelpers.countOnes(num) == ones)
+                return num;
+        }
+        return num;
+    }
+
+    @Test
+    public void testGetNextLargerBruteForce() {
+        assertEquals(78, getNextLargerBruteForce(77));
+        assertEquals(4, getNextLargerBruteForce(2));
+        assertEquals(9, getNextLargerBruteForce(6));
+        assertEquals(11, getNextLargerBruteForce(7));
+        assertEquals(16, getNextLargerBruteForce(8));
+    }
+
+    /**
+     * Function to get the next smaller number with the same number of one bits
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextSmaller(int num) {
+        int c = num;
+        int c0 = 0;
+        int c1 = 0;
+
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+
+        if (c == 0)
+            return -1;
+
+        while (((c & 1) == 0) && c != 0) {
+            c0++;
+            c >>= 1;
+        }
+
+        int p = c0 + c1;
+        num &= ((~0) << (p + 1));
+
+        int mask = (1 << (c1 + 1)) - 1; // zeros with 1 at position (c1+1)
+        num |= mask << (c0 - 1); // (c1+1) ones followed by (c0-1) zeros
+
+        return num;
+    }
+
+    @Test
+    public void testGetNextSmaller() {
+        assertEquals(75, getNextSmaller(77));
+        assertEquals(1, getNextSmaller(2));
+        assertEquals(5, getNextSmaller(6));
+        assertEquals(4, getNextSmaller(8));
+    }
+
+    /**
+     * Function to get the next larger number with the same number of one bits
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextLarger(int num) {
+        int c = num;
+        int c0 = 0;
+        int c1 = 0;
+        
+        while (((c & 1) == 0) && c != 0) {
+            c0++;
+            c >>= 1;
+        }
+
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+        // Numbers like 111111100000 don't have a larger number
+        if (c0 + c1 == 31 || c0 + c1 == 0)
+            return -1;
+
+        int p = c0 + c1; // position of rightmost non-trailing zero
+
+        num |= (1 << p); // flip the zero at p
+        num &= ~((1 << p) - 1); // clear all the bits to the right of this zero
+        num |= (1 << (c1 - 1)) - 1; // insert (c1-1) ones to the right of p such that ones are on the right
+
+        return num;
+    }
+
+    @Test
+    public void testGetNextLarger() {
+        assertEquals(78, getNextLarger(77));
+        assertEquals(4, getNextLarger(2));
+        assertEquals(9, getNextLarger(6));
+        assertEquals(11, getNextLarger(7));
+        assertEquals(16, getNextLarger(8));
+    }
+
+    /**
+     * Function to get the next smaller number with the same number of one bits
+     * (Arithmetic Approach)
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextSmallerArithmetic(int num) {
+        int c = num;
+        int c0 = 0;
+        int c1 = 0;
+
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+
+        if (c == 0)
+            return -1;
+
+        while (((c & 1) == 0) && c != 0) {
+            c0++;
+            c >>= 1;
+        }
+        return num - (1 << c1) - (1 << (c0 - 1)) + 1;
+    }
+
+    @Test
+    public void testGetNextSmallerArithmetic() {
+        assertEquals(75, getNextSmallerArithmetic(77));
+        assertEquals(1, getNextSmallerArithmetic(2));
+        assertEquals(5, getNextSmallerArithmetic(6));
+        assertEquals(4, getNextSmallerArithmetic(8));
+    }
+
+    /**
+     * Function to get the next larger number with the same number of one bits
+     * (Arithmetic Approach)
+     * 
+     * @param num
+     * @return
+     */
+    public int getNextLargerArithmetic(int num) {
+        int c = num;
+        int c0 = 0;
+        int c1 = 0;
+        while (((c & 1) == 0) && c != 0) {
+            c0++;
+            c >>= 1;
+        }
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+        return num + (1 << c0) + (1 << (c1 - 1)) - 1;
+    }
+
+    @Test
+    public void testGetNextLargerArithmetic() {
+        assertEquals(78, getNextLargerArithmetic(77));
+        assertEquals(4, getNextLargerArithmetic(2));
+        assertEquals(9, getNextLargerArithmetic(6));
+        assertEquals(11, getNextLargerArithmetic(7));
+        assertEquals(16, getNextLargerArithmetic(8));
+    }
+
+    /**
      * Function to count number of bits that need to be flipped to convert integer A
      * to integer B
      * 
@@ -84,12 +319,14 @@ public class BitManipulation {
             count++;
         return count;
     }
+
     @Test
-    public void testCountFlips(){
-        assertEquals(0, countFlips(7,7));
-        assertEquals(1, countFlips(63,55));
-        assertEquals(2, countFlips(29,15));
-        assertEquals(4, countFlips(213,28));
-        assertEquals(8, countFlips(255,0));
+    public void testCountFlips() {
+        assertEquals(0, countFlips(7, 7));
+        assertEquals(1, countFlips(63, 55));
+        assertEquals(2, countFlips(29, 15));
+        assertEquals(4, countFlips(213, 28));
+        assertEquals(8, countFlips(255, 0));
     }
+
 }
--- Chapter5/BitManipulationHelpers.java
+++ Chapter5/BitManipulationHelpers.java
@@ -99,11 +99,78 @@ public class BitManipulationHelpers {
         return num & mask;
     }
 
+    /**
+     * Function to clear bits from ith bit to jth bit(inclusive)
+     * 
+     * @param num
+     * @param i
+     * @param j
+     * @return
+     */
     public static int clearBitsithroughj(int num, int i, int j) {
-        int one= ~0;
+        int one = ~0;
         int left = one << (j + 1);
         int right = (1 << i) - 1;
         int mask = left | right;
         return num & mask;
+    }
+
+    /**
+     * Function to get one's complement of a number
+     * 
+     * @param num
+     * @return
+     */
+    public static int onesComplement(int num) {
+        int n = (int) (Math.floor(Math.log(num) / Math.log(2)) + 1);
+        return ((1 << n) - 1) ^ num;
+    }
+
+    /**
+     * Function that counts one bits in a number using Brian Kernighan’s algorithm
+     * 
+     * @param num
+     * @return
+     */
+    public static int countOnes(int num) {
+        int count = 0;
+        while (num != 0) {
+            num &= (num - 1);
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * Function to count zero bits in a number
+     * 
+     * @param num
+     * @return
+     */
+    public static int countZeros(int num) {
+        int n = onesComplement(num);
+        return countOnes(n);
+    }
+
+    public static void getZeroIndices(int num, int[] indices) {
+        int i = 0;
+        int j = 0;
+        while (i<Math.floor(Math.log(num) / Math.log(2))) {
+            if (!getBit(num, i)) {
+                indices[j] = i;
+                j++;
+            }
+            i++;
+        }
+    }
+
+    /**
+     * Function to check whether a number is a power of 2 or equals zero
+     * 
+     * @param num
+     * @return
+     */
+    public static boolean isPowerOfTwo(int num) {
+        return ((num & (num - 1)) == 0);
     }
 }
